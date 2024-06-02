b122ea640c815278c60ef3e914b88299aafcd2af
nicholaskim7 <154484441+nicholaskim7@users.noreply.github.com>
2024-06-01 22:56:51 -0500

    Update server.cpp

--- Client Server Approach/server.cpp
+++ Client Server Approach/server.cpp
@@ -1,185 +1,144 @@
 #include <iostream>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <vector>
-#include <string.h>
-#include <map>
-#include <cmath>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <queue>
 using namespace std;
 
-
-struct args
-{
-    vector<string> com;
-    map<string,char> m;
-    char* finalstr;
-};
-
-
-struct Node {
-    char c;
-    int freq;
-    Node *left, *right;
-
-    Node(char c, int freq) {
-        this->c = c;
-        this->freq = freq;
-        left = right = NULL;
+void handleRequest(int sockfd) {
+    int sMessage;
+    int n = read(sockfd, &sMessage, sizeof(int));
+    if (n < 0) {
+        std::cerr << "ERROR reading from socket";
+        exit(1);
     }
-};
 
+    char *buffer = new char[sMessage + 1];
+    bzero(buffer, sMessage + 1);
+    n = read(sockfd, buffer, sMessage);
+    if (n < 0) {
+        std::cerr << "ERROR reading from socket";
+        exit(1);
+    }
 
-struct compare {
-    bool operator()(Node* l, Node* r) {
-        if((l->freq == r->freq)){  
-            if(l->c == r->c){   
-                return l < r;  //tie breaker
+    std::string inputString(buffer);
+    delete[] buffer;
+
+    // Perform RLE encoding
+    std::string rleString;
+    std::vector<int> freq;
+
+    //char prevChar = inputString[0];
+    int count = 1;
+    
+    for (int i = 1; i <= inputString.length(); i++) {
+        if (i < inputString.length() && inputString[i] == inputString[i - 1]) {
+            count++;
+        } else {
+            if (count == 1) {
+                //if only one then just add the one char
+                rleString += inputString[i - 1];
+            } else {
+                //if count more than one than "running" add twice
+                rleString += inputString[i - 1];
+                rleString += inputString[i - 1];
+                freq.push_back(count);  //consider frequency
             }
-            return(l->c > r->c);  //compare ascii
+            count = 1; //reset count
         }
-        return (l->freq > r->freq);  //compare freq
-    } 
-};
-
-
-Node* HuffmanCodes(vector<char> data, vector<int> freq, int size) {
-    Node *left, *right, *top;
-
-    priority_queue<Node*, vector<Node*>, compare> minHeap;
-
-    for (int i = 0; i < size; i++)
-        minHeap.push(new Node(data[i], freq[i]));
-
-    while (minHeap.size() != 1) {
-        left = minHeap.top();
-        minHeap.pop();
-        right = minHeap.top();
-        minHeap.pop();
-        top = new Node(31, left->freq + right->freq);
-        top->left = left;
-        top->right = right;
-        minHeap.push(top);
     }
 
-    Node* root = minHeap.top();
-    return root;
-}
+    // Add the last character and its count
+    // rleString += prevChar;
+    // freq += std::to_string(count) + " ";
 
-map<string, char> printCodes(Node* root, string str, map<string, char> &m){
-    //create and return map containing code and symbol of that code
-    if(root->c != 31){
-        cout << "Symbol: " << root->c << ", Frequency: " << root->freq << ", Code: " << str << endl;
-        m[str] = root->c;  //create map
-    }
-    else{
-        printCodes(root->left, str + "0", m);
-        printCodes(root->right, str + "1", m);
-    }
-    return m;
-}
-
-
-//non-blocking manner, ensuring that no zombie processes are left unattended
-void fireman(int){
-    while (waitpid(-1, NULL, WNOHANG) > 0)
-        continue;
-}
-
-
-
-void error(char *msg){
-    perror(msg);
-    exit(1);
-}
-
-
-
-
-int main(int argc, char *argv[]){
-    string line;
-    char symb;
-    int freq;
-    int size = 0;
-    string c;
-    vector<char> alphas;
-    vector <int> freqs;
-    map<string, char> m;
-    while(getline(cin, line)){
-        symb = line[0];
-        freq = line[2] - '0';
-        alphas.push_back(symb);
-        freqs.push_back(freq);
-        size++;
-    }
-    Node* root = HuffmanCodes(alphas, freqs, size);
-    m = printCodes(root, c, m);
-
-    int sockfd, newsockfd, portno, clilen;
-
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
-    int n;
-
-    //if less than two arguments than port number was not provided
-    if(argc < 2){
-        cout << "Error no port provided" << endl;
+    // Return the RLE string and the frequency array to the client program
+    sMessage = static_cast<int>(rleString.length()) + 1; // Increment the size by one
+    n = write(sockfd, &sMessage, sizeof(int));
+    if (n < 0) {
+        std::cerr << "ERROR writing to socket";
         exit(1);
     }
 
-    //socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0){
-        error("Error opening socket");
+    n = write(sockfd, rleString.c_str(), sMessage);
+    if (n < 0) {
+        std::cerr << "ERROR writing to socket";
         exit(1);
     }
 
-    //used to zero out a block of memory
+    sMessage = static_cast<int>(freq.size()) * sizeof(int); // Increment the size by one
+    n = write(sockfd, &sMessage, sizeof(int));
+    if (n < 0) {
+        std::cerr << "ERROR writing to socket";
+        exit(1);
+    }
+
+    n = write(sockfd, freq.data(), sMessage);
+    if (n < 0) {
+        std::cerr << "ERROR writing to socket";
+        exit(1);
+    }
+
+    close(sockfd);
+    exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "usage: " << argv[0] << " port\n";
+        exit(1);
+    }
+
+    int portno = atoi(argv[1]);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        std::cerr << "ERROR opening socket";
+        exit(1);
+    }
+
+    struct sockaddr_in serv_addr;
     bzero((char *)&serv_addr, sizeof(serv_addr));
-
-    //portnumber
-    portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
-    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-        error("ERROR on binding");
-    //listen for incoming connections (5 is the max pending conections)
+
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "ERROR on binding";
+        exit(1);
+    }
+
     listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
 
-    //whenever a child process changes state (exits or stops), the fireman() function will be invoked to perform any necessary cleanup actions.
-    signal(SIGCHLD, fireman);
+    while (true) {
+        int newsockfd = accept(sockfd, NULL, NULL);
+        if (newsockfd < 0) {
+            std::cerr << "ERROR on accept";
+            exit(1);
+        }
 
-    while (true){  //infinate loop to continuously accept incomming client conections
-        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, (socklen_t *)&clilen);
-        if (fork() == 0){
-            if (newsockfd < 0)
-                error("new socket wont open");
-            bzero(buffer, 256);
-            //read data from the client and store in buffer
-            n = read(newsockfd, buffer, 255);
-            if (n < 0)
-                error("ERROR reading from socket");
-            string temp=buffer;  
-            //clear the buffer using bzero()
-            bzero(buffer, 256);
-            //setting up response which is stored in m
-            buffer[0] = m[temp];
-            //write to client
-            n = write(newsockfd, buffer, 255);
-            if (n < 0)
-                error("ERROR writing to socket");
+        // Create a new process to handle the client request
+        pid_t pid = fork();
+        if (pid < 0) {
+            std::cerr << "ERROR on fork";
+            exit(1);
+        }
+
+        if (pid == 0) {
+            // Child process
+            close(sockfd);
+            handleRequest(newsockfd);
+            exit(0);
+        } else {
+            // Parent process
             close(newsockfd);
-            _exit(0);
         }
     }
+
     close(sockfd);
     return 0;
 }
